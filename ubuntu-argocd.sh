1bfa5d7b19fdfd0e7713eb8932293755879da419
manojpodugu <psmk126@gmail.com>
2026-06-15 16:37:22 +0530

    Update ubuntu-argocd.sh

--- ubuntu-argocd.sh
+++ ubuntu-argocd.sh
@@ -14,8 +14,16 @@ kubectl apply --server-side \
 # Explanation for above command:  
      #1)For large CRDs such as applicationsets.argoproj.io, Kubernetes may reject them because the client-side apply mechanism stores a huge annotation "kubectl.kubernetes.io/last-applied-configuration". which exceeds the 256 KB limit.
      #2)We use server-side apply because it avoids storing the large "kubectl.kubernetes.io/last-applied-configuration" annotation, preventing large CRDs (like ApplicationSet) from failing with the Kubernetes annotation size limit.
-#------------ Installation done
-
+#------------ Installation done------------
+echo "=========================================================================================================="
+echo "==========================================================================================================="
+echo "kubectl get nodes -o wide  #To get ip address of worker nodes. EXTERNAL-IP are Public IP addresses. \n
+kubectl get svc argocd-server -n argocd -o wide #To get k8s port number like 32076."
+echo "=========================================================================================================="
+echo "==========================================================================================================="
+echo "kubectl get secret argocd-initial-admin-secret -n argocd -o yaml    # To get the value of the secret. \n
+echo "secret value" | base64 --decode         # To decode the secret. Because, secret you see encoded using base64."
+echo ""
 kubectl get all -n argocd # After installing the ArgoCD, to check what resources it has created.
 echo "=================================================================================================="
 echo "change ClusterIP to LoadBalancer"
