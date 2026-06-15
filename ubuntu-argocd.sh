f1a03f8fa8cb1bb5fd4b316b45137732420e727e
manojpodugu <psmk126@gmail.com>
2026-06-15 16:26:15 +0530

    Create ubuntu-argocd.sh

---
+++ ubuntu-argocd.sh
@@ -0,0 +1,35 @@
+#NOTE:
+#1) argocd installation only works with "kubectl".
+#2) To use kubectl, we need created cluster first.
+
+#Installation steps
+
+kubectl create namespace argocd  #Create the namespace for argoCD
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml  #Install ArgoCD
+kubectl apply --server-side \
+-n argocd \
+-f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml  # Install some dependencies, which are needed to avoid "Pod CrashLoopBackOff Error".
+
+
+# Explanation for above command:  
+     #1)For large CRDs such as applicationsets.argoproj.io, Kubernetes may reject them because the client-side apply mechanism stores a huge annotation "kubectl.kubernetes.io/last-applied-configuration". which exceeds the 256 KB limit.
+     #2)We use server-side apply because it avoids storing the large "kubectl.kubernetes.io/last-applied-configuration" annotation, preventing large CRDs (like ApplicationSet) from failing with the Kubernetes annotation size limit.
+#------------ Installation done
+
+kubectl get all -n argocd # After installing the ArgoCD, to check what resources it has created.
+echo "=================================================================================================="
+echo "change ClusterIP to LoadBalancer"
+echo "==================================================================================================="
+kubectl edit svc argocd-server -n argocd # entering into service file with "ClusterIP" type.
+
+
+#reference when file is opened. Find the below pattern and change "ClusterIP" to "NodePort" or "LoadBalancer"(recommended). After save and exit.
+
+ #selector:
+ #   app.kubernetes.io/name: argocd-server
+ # sessionAffinity: None
+ # type: ClusterIP
+#status:
+#  loadBalancer: {}
+
+#--------------------reference ends here--------------------
