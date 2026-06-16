157b01afe7a3bd7195405fa393a3257ef8630cce
manojpodugu <psmk126@gmail.com>
2026-06-16 14:41:08 +0530

    Create ubuntu-tomcat.sh

---
+++ ubuntu-tomcat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install openjdk-17-jdk -y
+
+cd /opt
+sudo wget https://downloads.apache.org/tomcat/tomcat-10/v10.1.43/bin/apache-tomcat-10.1.43.tar.gz
+
+sudo tar -xzf apache-tomcat-10.1.43.tar.gz
+sudo mv apache-tomcat-10.1.43 tomcat
+
+sudo chmod +x /opt/tomcat/bin/*.sh
+
+/opt/tomcat/bin/startup.sh
