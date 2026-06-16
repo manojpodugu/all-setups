b77357cc8ac478e8185ef7ddfc0647614ef7c130
manojpodugu <psmk126@gmail.com>
2026-06-16 12:11:53 +0530

    Update ubuntu-nexus.sh

--- ubuntu-nexus.sh
+++ ubuntu-nexus.sh
@@ -8,7 +8,7 @@ sudo apt update -y
 echo "Installing required packages..."
 sudo apt install -y wget tar
 
-cd /opt
+cd /mynexus
 
 NEXUS_VERSION="3.93.0-06"
 
@@ -23,17 +23,17 @@ NEXUS_DIR=$(ls -d nexus-* | head -1)
 sudo mv $NEXUS_DIR nexus
 
 echo "Creating nexus user..."
-sudo useradd -r -m -d /opt/nexus -s /bin/bash nexus || true
+sudo useradd -r -m -d /mynexus/nexus -s /bin/bash nexus || true
 
 echo "Setting ownership..."
-sudo chown -R nexus:nexus /opt/nexus
+sudo chown -R nexus:nexus /mynexus/nexus
 
-if [ -d /opt/sonatype-work ]; then
-    sudo chown -R nexus:nexus /opt/sonatype-work
+if [ -d /mynexus/sonatype-work ]; then
+    sudo chown -R nexus:nexus /mynexus/sonatype-work
 fi
 
 echo "Configuring Nexus user..."
-sudo sed -i 's/#run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
+sudo sed -i 's/#run_as_user=""/run_as_user="nexus"/' /mynexus/nexus/bin/nexus.rc
 
 echo "Creating systemd service..."
 
@@ -47,8 +47,8 @@ Type=forking
 LimitNOFILE=65536
 User=nexus
 Group=nexus
-ExecStart=/opt/nexus/bin/nexus start
-ExecStop=/opt/nexus/bin/nexus stop
+ExecStart=/mynexus/nexus/bin/nexus start
+ExecStop=/mynexus/nexus/bin/nexus stop
 Restart=on-abort
 
 [Install]
