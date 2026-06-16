9c8a1b882f4b335d64e3ca93a60d9b36999b155a
manojpodugu <psmk126@gmail.com>
2026-06-16 16:51:07 +0530

    Refactor ubuntu-tomcat.sh for clarity and functionality
    Updated script to remove unnecessary sudo commands and added echo statements for better user feedback.

--- ubuntu-tomcat.sh
+++ ubuntu-tomcat.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo -e
-sudo apt install openjdk-17-jdk -y
+set -e
+
+apt update -y
+apt install openjdk-17-jdk wget -y
+
+TOMCAT_VERSION=10.1.55
 
 cd /opt
 
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.43/bin/apache-tomcat-10.1.43.tar.gz
+echo "Downloading Tomcat..."
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
 
-sudo tar -xzf apache-tomcat-10.1.43.tar.gz
-sudo mv apache-tomcat-10.1.43 tomcat
+echo "Extracting..."
+tar -xvzf apache-tomcat-${TOMCAT_VERSION}.tar.gz
 
-sudo chmod +x /opt/tomcat/bin/*.sh
+mv apache-tomcat-${TOMCAT_VERSION} tomcat
 
+chmod +x /opt/tomcat/bin/*.sh
+
+echo "Starting Tomcat..."
 /opt/tomcat/bin/startup.sh
+
+echo "SUCCESS 🚀"
+echo "Open: http://<EC2-PUBLIC-IP>:8080"
