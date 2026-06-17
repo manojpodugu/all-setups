c32c85692f6a51bedb66e851d26a9984ef194db3
manojpodugu <psmk126@gmail.com>
2026-06-17 11:10:15 +0530

    Update tomcat-portchange.sh

--- tomcat-portchange.sh
+++ tomcat-portchange.sh
@@ -6,7 +6,16 @@ NEW_PORT=8888
 # " sed -i '/<condition>/ s/<search-pattern>/<replacement>/' <file> " SYNTAX of below command.
 sed -i '/protocol="HTTP\/1.1"/ s/port="[0-9]\+"/port="'"$NEW_PORT"'"/' /root/tomcat/conf/server.xml
 
+# Stop Tomcat if installed
+if [ -f /root/tomcat/bin/shutdown.sh ]; then
+    echo "========================= Tomcat installation found. Stopping Tomcat...================================="
+    sh /root/tomcat/bin/shutdown.sh || true
+    sleep 5
+else
+    echo "==================== Tomcat installation not found. Skipping shutdown.sh ================================"
+fi
 
+echo "Starting Tomcat..."
 sh /root/tomcat/bin/startup.sh
 
 echo "Tomcat Port    : $NEW_PORT"
