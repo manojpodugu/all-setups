074bc1276ac4751411a965bb6a42061d4e16fb9d
manojpodugu <psmk126@gmail.com>
2026-06-17 08:01:41 +0530

    Update ubuntu-tomcat.sh

--- ubuntu-tomcat.sh
+++ ubuntu-tomcat.sh
@@ -7,7 +7,7 @@ apt install openjdk-17-jdk wget -y
 
 TOMCAT_VERSION=10.1.55
 
-cd /opt
+cd /root || true
 
 echo "Downloading Tomcat..."
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
@@ -20,14 +20,14 @@ mv apache-tomcat-${TOMCAT_VERSION} tomcat
 # FIX: Proper ownership & permissions (CRITICAL FIX)  # use if necessary.
 # ---------------------------
 #echo "Setting permissions..."
-#chown -R root:root /opt/tomcat
-#chmod -R 755 /opt/tomcat
-#chmod -R 755 /opt/tomcat/webapps
+#chown -R root:root /root/tomcat
+#chmod -R 755 /root/tomcat
+#chmod -R 755 /root/tomcat/webapps
 
 # ---------------------------
 # 1. Add users dynamically. You are: Adding role: manager-gui, Adding role: manager-script, Creating user: admin, Giving that user permissions
 # ---------------------------
-TOMCAT_USERS="/opt/tomcat/conf/tomcat-users.xml"
+TOMCAT_USERS="/root/tomcat/conf/tomcat-users.xml"
 
 # Insert roles before closing tag
 sed -i '/<\/tomcat-users>/ i \
@@ -38,14 +38,14 @@ $TOMCAT_USERS
 # Remove restrictive Valve entry
 echo "=============== Removing <Valve className="org.apache.catalina.valves.RemoteCIDRValve" allow="127.0.0.0/8,::1/128" /> ================================================ "
          
-sed -i '/<Valve className="org.apache.catalina.valves.RemoteAddrValve"/,/\/>$/d' /opt/tomcat/webapps/manager/META-INF/context.xml
-echo " updated /opt/tomcat/webapps/manager/META-INF/context.xml"
-chmod +x /opt/tomcat/bin/*.sh
+sed -i '/<Valve className="org.apache.catalina.valves.RemoteAddrValve"/,/\/>$/d' /root/tomcat/webapps/manager/META-INF/context.xml
+echo " updated /root/tomcat/webapps/manager/META-INF/context.xml"
+chmod +x /root/tomcat/bin/*.sh
 
 echo "Starting Tomcat..."
-/opt/tomcat/bin/startup.sh
+/root/tomcat/bin/startup.sh
 
 echo "SUCCESS 🚀"
 echo "Open: http://<EC2-PUBLIC-IP>:8080"
-echo "vi /opt/tomcat/conf/tomcat-users.xml"
-echo "vi /opt/tomcat/webapps/manager/META-INF/context.xml"
+echo "vi /root/tomcat/conf/tomcat-users.xml"
+echo "vi /root/tomcat/webapps/manager/META-INF/context.xml"
