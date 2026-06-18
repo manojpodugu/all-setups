28984a55beedb94deddca4242e996740033c68a1
manojpodugu <psmk126@gmail.com>
2026-06-18 17:34:14 +0530

    Fix installation message for Ansible

--- ubuntu-ansible.sh
+++ ubuntu-ansible.sh
@@ -4,8 +4,10 @@ set -e
 echo "Updating system..."
 apt update && apt upgrade -y
 
-echo "Installing Ansible + SSH tools..."
+echo "Installing Ansible"
 sudo apt install ansible -y
 
 echo "Done on Ansible control node"
 ansible --version
+
+echo "======================================================== Ansible Installed Successfully ================================================================="
