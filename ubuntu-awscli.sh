af9b9f5a88f1a8bcc4960d4eeb5de5ac97f1afa0
manojpodugu <psmk126@gmail.com>
2026-06-19 19:22:12 +0530

    Create ubuntu-awscli.sh

---
+++ ubuntu-awscli.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+echo "Updating system..."
+sudo apt update -y
+sudo apt updrage -y
+
+echo "Installing prerequisites..."
+sudo apt install -y curl unzip tar
+
+echo "Installing AWS CLI v2..."
+curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+sudo ./aws/install
+
+aws --version
